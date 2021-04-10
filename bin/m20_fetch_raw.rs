f3d6fbd1d4823119c611a5721eaa371ca428e030
Kevin M. Gill <kmsmgill@gmail.com>
2021-04-10 15:25:33 -0700

    Connected commandline arguments to json list request

--- bin/m20_fetch_raw.rs
+++ bin/m20_fetch_raw.rs
@@ -1,5 +1,5 @@
 
-use mars_raw_utils::{constants, print, vprintln, jsonfetch, httpfetch, path};
+use mars_raw_utils::{constants, print, vprintln, jsonfetch, httpfetch, path, util};
 use json::{JsonValue};
 use std::path::Path;
 use std::fs::File;
@@ -42,8 +42,8 @@ fn fetch_image(image:&JsonValue) {
     let bn = path::basename(image_url);
 
     let image_data = httpfetch::simple_fetch_bin(image_url).unwrap();
-
-    let path = Path::new("/home/kgill/Desktop").join(bn.to_string());
+    
+    let path = Path::new(bn.as_str());
 
     let mut file = match File::create(&path) {
         Err(why) => panic!("couldn't create {}", why),
@@ -94,12 +94,14 @@ fn main() {
                     .long("list")
                     .value_name("list")
                     .help("Don't download, only list results")
+                    .takes_value(false)
                     .required(false)) 
                 .arg(Arg::with_name("thumbnails")
                     .short("t")
                     .long("thumbnails")
                     .value_name("thumbnails")
                     .help("Download thumbnails in the results")
+                    .takes_value(false)
                     .required(false)) 
                 .arg(Arg::with_name("num")
                     .short("n")
@@ -125,33 +127,104 @@ fn main() {
                 .get_matches();
 
 
-    /*
-        parser.add_argument("-c", "--camera", help="MSL Camera Instrument(s)", required=None, default=None, type=str, nargs='+', choices=build_choices_list())
-    parser.add_argument("-s", "--sol", help="Mission Sol", required=False, type=int)
-    parser.add_argument("-m", "--minsol", help="Starting Mission Sol", required=False, type=int)
-    parser.add_argument("-M", "--maxsol", help="Ending Mission Sol", required=False, type=int)
-    parser.add_argument("-l", "--list", help="Don't download, only list results", action="store_true")
-    parser.add_argument("-r", "--raw", help="Print raw JSON response", action="store_true")
-    parser.add_argument("-t", "--thumbnails", help="Download thumbnails in the results", action="store_true")
-    parser.add_argument("-n", "--num", help="Max number of results", required=False, type=int, default=100)
-    parser.add_argument("-p", "--page", help="Results page (starts at 1)", required=False, type=int, default=1)
-    parser.add_argument("-S", "--seqid", help="Specific sequence id", required=False, type=str, default=None)
-    */
-
     if matches.is_present(constants::param::PARAM_VERBOSE) {
         print::set_verbose(true);
     }
+
+    let mut num_per_page = 100;
+    let mut page = 1;
+    let mut minsol = 1000000;
+    let mut maxsol = -1;
+    let mut sol = -1;
+    let mut thumbnails = false;
+    let mut search = "";
+    let mut list_only = false;
+
+    let cameras: Vec<&str> = matches.values_of("camera").unwrap().collect();
+
+    if matches.is_present("thumbnails") {
+        thumbnails = true;
+    }
+
+    if matches.is_present("list") {
+        list_only = true;
+    }
+
+    if matches.is_present("seqid") {
+        search =  matches.value_of("seqid").unwrap();
+    }
+
+    if matches.is_present("num") {
+        let s = matches.value_of("num").unwrap();
+        if util::string_is_valid_f32(&s) {
+            num_per_page = s.parse::<i32>().unwrap();
+        } else {
+            eprintln!("Error: Invalid number specified");
+            process::exit(1);
+        }
+    }
+
+    if matches.is_present("page") {
+        let s = matches.value_of("page").unwrap();
+        if util::string_is_valid_f32(&s) {
+            page = s.parse::<i32>().unwrap();
+        } else {
+            eprintln!("Error: Invalid number specified");
+            process::exit(1);
+        }
+    }
+
+    if matches.is_present("minsol") {
+        let s = matches.value_of("minsol").unwrap();
+        if util::string_is_valid_f32(&s) {
+            minsol = s.parse::<i32>().unwrap();
+        } else {
+            eprintln!("Error: Invalid number specified");
+            process::exit(1);
+        }
+    }
+
+    if matches.is_present("maxsol") {
+        let s = matches.value_of("maxsol").unwrap();
+        if util::string_is_valid_f32(&s) {
+            maxsol = s.parse::<i32>().unwrap();
+        } else {
+            eprintln!("Error: Invalid number specified");
+            process::exit(1);
+        }
+    }
+
+    if matches.is_present("sol") {
+        let s = matches.value_of("sol").unwrap();
+        if util::string_is_valid_f32(&s) {
+            sol = s.parse::<i32>().unwrap();
+        } else {
+            eprintln!("Error: Invalid number specified");
+            process::exit(1);
+        }
+    }
+
+    if sol >= 0 {
+        minsol = sol;
+        maxsol = sol;
+    }
+
+    let joined_cameras = cameras.join("|");
+    let num_per_page_s = format!("{}", num_per_page);
+    let page_s = format!("{}", (page - 1));
+    let minsol_s = format!("{}:sol:gte", minsol);
+    let maxsol_s = format!("{}:sol:lte", maxsol);
 
     let params = vec![
         vec!["feed", "raw_images"],
         vec!["category", "mars2020"],
         vec!["feedtype", "json"],
-        vec!["num", "2"],
-        vec!["page", "0"],
+        vec!["num", num_per_page_s.as_str()],
+        vec!["page", page_s.as_str()],
         vec!["order", "sol desc"],
-        vec!["search", "NAVCAM_RIGHT"],
-        vec!["condition_2", "43:sol:gte"],
-        vec!["condition_3", "46:sol:lte"]
+        vec!["search", joined_cameras.as_str()],
+        vec!["condition_2", minsol_s.as_str()],
+        vec!["condition_3", maxsol_s.as_str()]
     ];
 
     let uri = constants::url::M20_RAW_WEBSERVICE_URL;
@@ -168,8 +241,21 @@ fn main() {
     for i in 0..json_res["images"].len() {
         let image = &json_res["images"][i];
         
+        // If this image is a thumbnail and we're ignoring those, then ignore it.
+        if image["sample_type"] == "Thumbnail" && ! thumbnails {
+            continue;
+        }
+
+        // If we're searching for a substring and this image doesn't match, skip it.
+        if search != "" && image["imageid"].as_str().unwrap().find(&search) == None {
+            continue;
+        }
+
         print_image(image);
-        fetch_image(image);
+
+        if !list_only {
+            fetch_image(image);
+        }
         
     }
 
