697023e364889350aa83ba571cb9fa6085b84f17
Kevin M. Gill <kmsmgill@gmail.com>
2021-04-19 09:30:35 -0700

    Cleaned opencv stuff from inpaint. Nothing works now

--- src/inpaint.rs
+++ src/inpaint.rs
@@ -1,12 +1,6 @@
 
 // https://www.researchgate.net/publication/238183352_An_Image_Inpainting_Technique_Based_on_the_Fast_Marching_Method
 
-use opencv::{
-    core,
-    imgcodecs, 
-    photo
-};
-use opencv::prelude::MatTraitManual;
 
 use crate::{
     constants, 
@@ -15,7 +9,8 @@ use crate::{
     enums, 
     imagebuffer::ImageBuffer, 
     vprintln,
-    opencvutils
+    opencvutils,
+    not_implemented
 };
 
 
@@ -45,46 +40,48 @@ pub fn inpaint_supported_for_instrument(instrument:enums::Instrument) -> bool {
     }
 }
 
-fn load_mask_file(filename:&str, instrument:enums::Instrument) -> error::Result<core::Mat> {
+fn load_mask_file(filename:&str, instrument:enums::Instrument) -> error::Result<ImageBuffer> {
     vprintln!("Loading inpaint mask file {}", filename);
 
     if ! path::file_exists(filename) {
         return Err(constants::status::FILE_NOT_FOUND);
     }
-
-    let mask = imgcodecs::imread(filename, imgcodecs::IMREAD_GRAYSCALE).unwrap();
-
+    let mask = ImageBuffer::from_file(filename).unwrap();
+    
     match instrument {
-        enums::Instrument::MslMAHLI => Ok(opencvutils::crop(&mask, 32, 16, 1584, 1184).unwrap()),
+        enums::Instrument::MslMAHLI => Ok(mask.get_subframe(32, 16, 1584, 1184).unwrap()),
         _ => Ok(mask)
     }
 }
 
-fn load_mask(instrument:enums::Instrument) -> error::Result<core::Mat> {
+fn load_mask(instrument:enums::Instrument) -> error::Result<ImageBuffer> {
     let mask_file = determine_mask_file(instrument).unwrap();
     load_mask_file(mask_file, instrument)
 }
+
 
 pub fn apply_inpaint_to_buffer(buffer:&ImageBuffer, instrument:enums::Instrument) -> error::Result<ImageBuffer> {
 
     let mut mask = load_mask(instrument).unwrap();
 
-    let sz = mask.size().unwrap();
-
     // Crop the mask image if it's larger than the input image. 
     // Sizes need to match
-    if sz.width > buffer.width as i32 {
-        let x = (sz.width - buffer.width as i32) / 2;
-        let y = (sz.height - buffer.width as i32) / 2;
+    if mask.width > buffer.width {
+        let x = (mask.width - buffer.width) / 2;
+        let y = (mask.height - buffer.width) / 2;
         vprintln!("Cropping inpaint mask with params {}, {}, {}, {}", x, y, buffer.width, buffer.height);
-        mask = opencvutils::crop(&mask, x, y, buffer.width as i32, buffer.height as i32).unwrap();
+        mask = mask.get_subframe(x, y, buffer.width, buffer.height).unwrap();
     }
     let buffer_as_mat = opencvutils::buffer_to_cv2_mat(&buffer).unwrap();
 
-    unsafe {
-        let mut dest = core::Mat::new_rows_cols(buffer.height as i32, buffer.width as i32, core::CV_8U).unwrap();
-        photo::inpaint(&buffer_as_mat, &mask, &mut dest, 3.0, photo::INPAINT_TELEA).unwrap();
-        let b = opencvutils::cv2_mat_to_buffer(&dest, buffer.width, buffer.height).unwrap();
-        Ok(b)
-    }
+
+    not_implemented!()
+    // unsafe {
+    //     let mut dest = core::Mat::new_rows_cols(buffer.height as i32, buffer.width as i32, core::CV_8U).unwrap();
+    //     photo::inpaint(&buffer_as_mat, &mask, &mut dest, 3.0, photo::INPAINT_TELEA).unwrap();
+    //     let b = opencvutils::cv2_mat_to_buffer(&dest, buffer.width, buffer.height).unwrap();
+    //     Ok(b)
+    // }
 }
+
+
