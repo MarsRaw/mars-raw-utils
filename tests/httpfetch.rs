da2d5cd5b1fa502679cb63675e9ad8d8da7bc066
Kevin M. Gill <kmsmgill@gmail.com>
2021-04-26 19:44:41 -0700

    Added tests for jsonfetch and httpfetch

---
+++ tests/httpfetch.rs
@@ -0,0 +1,67 @@
+use mars_raw_utils::{
+    httpfetch
+};
+
+use json;
+
+#[test]
+fn test_text_fetch() {
+
+    let hf = httpfetch::HttpFetcher::new("http://echo.jsontest.com/key/value/one/two");
+    let res = hf.fetch_text().unwrap();
+
+    assert_eq!(res, "{
+   \"one\": \"two\",
+   \"key\": \"value\"
+}
+");
+}
+
+#[test]
+fn test_text_fetch_with_params() {
+
+    let mut hf = httpfetch::HttpFetcher::new("http://validate.jsontest.com/");
+    hf.param("json", "{\"foo\":\"bar\"}");
+    let res = hf.fetch_text().unwrap();
+
+    let j = json::parse(&res).unwrap();
+    assert_eq!(j["object_or_array"], "object");
+    assert_eq!(j["size"], 1);
+}
+
+#[test]
+fn test_bin_fetch() {
+
+    let hf = httpfetch::HttpFetcher::new("http://echo.jsontest.com/key/value/one/two");
+    let res = hf.fetch_bin().unwrap();
+
+    assert_eq!(std::str::from_utf8(&res[..]).unwrap(), "{
+   \"one\": \"two\",
+   \"key\": \"value\"
+}
+");
+}
+
+#[test]
+fn test_simple_text_fetch() {
+
+    let res = httpfetch::simple_fetch_text("http://echo.jsontest.com/key/value/one/two").unwrap();
+
+    assert_eq!(res, "{
+   \"one\": \"two\",
+   \"key\": \"value\"
+}
+");
+}
+
+#[test]
+fn test_simple_bin_fetch() {
+
+    let res = httpfetch::simple_fetch_bin("http://echo.jsontest.com/key/value/one/two").unwrap();
+
+    assert_eq!(std::str::from_utf8(&res[..]).unwrap(), "{
+   \"one\": \"two\",
+   \"key\": \"value\"
+}
+");
+}
---
+++ tests/jsonfetch.rs
@@ -0,0 +1,21 @@
+use mars_raw_utils::{
+    jsonfetch
+};
+
+#[test]
+fn test_json_fetch() {
+
+    let jf = jsonfetch::JsonFetcher::new("http://echo.jsontest.com/key/value/one/two");
+    let res = jf.fetch().unwrap();
+    assert_eq!(res["one"], "two");
+    assert_eq!(res["key"], "value");
+}
+
+#[test]
+fn test_json_fetch_with_params() {
+    let mut jf = jsonfetch::JsonFetcher::new("http://validate.jsontest.com/");
+    jf.param("json", "{\"foo\":\"bar\"}");
+    let res = jf.fetch().unwrap();
+    assert_eq!(res["object_or_array"], "object");
+    assert_eq!(res["size"], 1);
+}
