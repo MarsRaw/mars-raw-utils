8ee80fb6b5fc74bd33d41aaacd7bde8c0381e52a
Kevin M. Gill <kmsmgill@gmail.com>
2022-02-16 14:05:02 -0800

    A little refactoring. Start towards supporting color

--- bin/diffgif.rs
+++ bin/diffgif.rs
@@ -23,14 +23,14 @@ use clap::{Arg, App};
 use std::process;
 
 
-fn imagebuffer_to_vec_v8(buff:&imagebuffer::ImageBuffer) -> Vec<u8> {
-    let mut f : Vec<u8> = vec!(0; buff.width * buff.height * 3);
-    for y in 0..buff.height {
-        for x in 0..buff.width {
-            let idx = (y * buff.width + x) * 3;
-            f[idx + 0] = buff.get(x, y).unwrap().round() as u8;
-            f[idx + 1] = f[idx + 0];
-            f[idx + 2] = f[idx + 0];
+fn imagebuffer_to_vec_v8(buff_0:&imagebuffer::ImageBuffer, buff_1:&imagebuffer::ImageBuffer, buff_2:&imagebuffer::ImageBuffer) -> Vec<u8> {
+    let mut f : Vec<u8> = vec!(0; buff_0.width * buff_0.height * 3);
+    for y in 0..buff_0.height {
+        for x in 0..buff_0.width {
+            let idx = (y * buff_0.width + x) * 3;
+            f[idx + 0] = buff_0.get(x, y).unwrap().round() as u8;
+            f[idx + 1] = buff_1.get(x, y).unwrap().round() as u8;
+            f[idx + 2] = buff_2.get(x, y).unwrap().round() as u8;
         }
     }
 
@@ -76,6 +76,102 @@ fn generate_mean_stack(input_files:&Vec<&str>) -> rgbimage::RgbImage {
     }
 
     mean
+}
+
+
+fn process_band(band:&imagebuffer::ImageBuffer, mean_band:&imagebuffer::ImageBuffer, black_level:f32, white_level:f32, gamma:f32, blur_kernel_size:f32) -> imagebuffer::ImageBuffer {
+    let diff = band.subtract(mean_band).unwrap();
+    let mut d = diff.clone();
+
+    // Convert for absolute value difference
+    for y in 0..d.height {
+        for x in 0..d.width {
+            let v = d.get(x, y).unwrap();
+            d.put(x, y, v.abs());
+        }
+    }
+
+    let mm = d.get_min_max().unwrap();
+    let rng = 65535.0;
+    let norm_min = (rng * black_level) + mm.min;
+    let norm_max = (rng * white_level) + mm.min;
+    
+    d.clip_mut(norm_min, norm_max);
+    d.power_mut(gamma);
+
+    let mut n = d.normalize(0.0, 65535.0).unwrap();
+
+    for y in 0..d.height {
+        for x in 0..d.width {
+            let mult = match diff.get(x, y).unwrap() >= 0.0 {
+                true => 1.0,
+                false => -1.0
+            };
+            n.put(x, y, n.get(x, y).unwrap() * mult);
+        }
+    }
+
+    let blurred = match blur_kernel_size == 0.0 {
+        true => n.clone(),
+        false => {
+            // This method is lossy. Get over it.
+            // So if we're dealing with negative numbers here, we
+            // will need to scale them to within range of a u16.
+            // To do that, we will scale all values by half, then
+            // add the absolute value of the lowest value. 
+            // Then do the blur
+            // Then undo that offset and scaling. 
+            // We lose precision by about half
+            
+            let mnmx = n.get_min_max().unwrap();
+            let init_mn = mnmx.min;
+            if init_mn < 0.0 {
+                n.scale_mut(0.5);
+                n.add_across_mut(init_mn.abs() * 0.5);
+            }
+            
+            let mut b = blur::blur_imagebuffer(&n, blur_kernel_size);
+
+            if init_mn < 0.0 {
+                b.subtract_across_mut(init_mn.abs() * 0.5);
+                b.scale_mut(2.0);
+            }
+            
+            b
+        }
+    };
+
+
+    
+
+    let mut merged = mean_band.add(&blurred).unwrap();
+    merged.clip_mut(0.0, 65355.0);
+    merged
+}
+
+fn process_file(encoder:&mut gif::Encoder<&mut std::fs::File>, in_file:&String, mean_stack:&rgbimage::RgbImage, black_level:f32, white_level:f32, gamma:f32, blur_kernel_size:f32, delay:u16) {
+    vprintln!("Processing frame differential on file: {}", in_file);
+
+    let raw = rgbimage::RgbImage::open16(&in_file).unwrap();
+
+    let mut processed_band_0 = process_band(&raw.get_band(0), &mean_stack.get_band(0), black_level, white_level, gamma, blur_kernel_size);
+    let mut processed_band_1 = process_band(&raw.get_band(1), &mean_stack.get_band(1), black_level, white_level, gamma, blur_kernel_size);
+    let mut processed_band_2 = process_band(&raw.get_band(2), &mean_stack.get_band(1), black_level, white_level, gamma, blur_kernel_size);
+
+    // TODO:
+    // _ Absolute difference
+    // _ Add back to the mean
+    // _ Multiband (RGB)
+    // _ Refactor for cleanliness
+
+    processed_band_0.normalize_mut(0.0, 255.0);
+    processed_band_1.normalize_mut(0.0, 255.0);
+    processed_band_2.normalize_mut(0.0, 255.0);
+
+    let mut pixels = imagebuffer_to_vec_v8(&processed_band_0, &processed_band_1, &processed_band_2);
+    let mut frame = gif::Frame::from_rgb(raw.width as u16, raw.height as u16, &mut *pixels);
+    frame.delay = delay;
+    encoder.write_frame(&frame).unwrap();
 }
 
 fn main() {
@@ -258,92 +354,7 @@ fn main() {
     
     for in_file in input_files.iter() {
         if path::file_exists(in_file) {
-            vprintln!("Processing frame differential on file: {}", in_file);
-
-            let raw = rgbimage::RgbImage::open16(&String::from(*in_file)).unwrap();
-
-            let b0 = raw.get_band(0);
-            let m0 = mean_stack.get_band(0);
-            
-            let diff = b0.subtract(m0).unwrap();
-            let mut d = diff.clone();
-
-            // Convert for absolute value difference
-            for y in 0..d.height {
-                for x in 0..d.width {
-                    let v = d.get(x, y).unwrap();
-                    d.put(x, y, v.abs());
-                }
-            }
-
-            let mm = d.get_min_max().unwrap();
-            let rng = 65535.0;
-            let norm_min = (rng * black_level) + mm.min;
-            let norm_max = (rng * white_level) + mm.min;
-            
-            d.clip_mut(norm_min, norm_max);
-            d.power_mut(gamma);
-
-            let mut n = d.normalize(0.0, 65535.0).unwrap();
-
-            for y in 0..d.height {
-                for x in 0..d.width {
-                    let mult = match diff.get(x, y).unwrap() >= 0.0 {
-                        true => 1.0,
-                        false => -1.0
-                    };
-                    n.put(x, y, n.get(x, y).unwrap() * mult);
-                }
-            }
-
-            let blurred = match blur_kernel_size == 0.0 {
-                true => n.clone(),
-                false => {
-                    // This method is lossy. Get over it.
-                    // So if we're dealing with negative numbers here, we
-                    // will need to scale them to within range of a u16.
-                    // To do that, we will scale all values by half, then
-                    // add the absolute value of the lowest value. 
-                    // Then do the blur
-                    // Then undo that offset and scaling. 
-                    // We lose precision by about half
-                    
-                    let mnmx = n.get_min_max().unwrap();
-                    let init_mn = mnmx.min;
-                    if init_mn < 0.0 {
-                        n.scale_mut(0.5);
-                        n.add_across_mut(init_mn.abs() * 0.5);
-                    }
-                    
-                    let mut b = blur::blur_imagebuffer(&n, blur_kernel_size);
-
-                    if init_mn < 0.0 {
-                        b.subtract_across_mut(init_mn.abs() * 0.5);
-                        b.scale_mut(2.0);
-                    }
-                    
-                    b
-                }
-            };
-
-
-            
-
-            let mut merged = m0.add(&blurred).unwrap();
-            merged.clip_mut(0.0, 65355.0);
-
-            // TODO:
-            // _ Absolute difference
-            // _ Add back to the mean
-            // _ Multiband (RGB)
-            // _ Refactor for cleanliness
-
-            merged.normalize_mut(0.0, 255.0);
-            let mut pixels = imagebuffer_to_vec_v8(&merged);
-            let mut frame = gif::Frame::from_rgb(raw.width as u16, raw.height as u16, &mut *pixels);
-            frame.delay = delay;
-            encoder.write_frame(&frame).unwrap();
-            
+            process_file(&mut encoder, &String::from(*in_file), &mean_stack, black_level, white_level, gamma, blur_kernel_size, delay);
         } else {
             eprintln!("File not found: {}", in_file);
         }
