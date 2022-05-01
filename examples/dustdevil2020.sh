26bf06e7c8973675afc7732cbbb1da55c5ef200c
Kevin M. Gill <kmsmgill@gmail.com>
2022-05-01 10:10:46 -0700

    Changed NCAM to ECAM

--- examples/dustdevil2020.sh
+++ examples/dustdevil2020.sh
@@ -14,17 +14,17 @@ if [ $soldir -lt 1000 ]; then
     soldir=0${soldir}
 fi
 
-echo Processing in directory ${soldir}/NCAM
+echo Processing in directory ${soldir}/ECAM
 
-if [ ! -d 0${soldir}/NCAM ]; then
-    mkdir -p ${soldir}/NCAM
+if [ ! -d 0${soldir}/ECAM ]; then
+    mkdir -p ${soldir}/ECAM
 fi
 
-cd ${soldir}/NCAM 
+cd ${soldir}/ECAM 
 
 m20_fetch_raw -c NAVCAM_LEFT -s $sol -S ${seqid}
 
-m20_ecam_calibrate -i *J0?.png 
+calibrate -i *J0?.png 
 
 diffgif -i *${seqid}*-rjcal.png -o DiffGif_${sol}_${seqid}.gif -v -b 0 -w 3.0 -g 1.0 -l 5 -d 20 -p stacked
 
