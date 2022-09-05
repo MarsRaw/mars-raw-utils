b252117f78477cc196b0a41554863ae8c196c0ed
Kevin M. Gill <kmsmgill@gmail.com>
2022-09-05 16:06:27 -0700

    Issue #24: mostly implemented cahv linearization for cross-eye

--- bin/subs/xeye.rs
+++ bin/subs/xeye.rs
@@ -3,7 +3,8 @@ use mars_raw_utils::{
     prelude::*
 };
 use sciimg::{
-    prelude::*
+    prelude::*,
+    vector::Vector
 };
 use image::{
     load_from_memory
@@ -63,6 +64,133 @@ impl OpenFromBytes for RgbImage {
     }
 }
 
+trait GetCameraModel {
+    fn get_camera_model(&self) -> Option<CameraModel>;
+    fn has_camera_model(&self) -> bool;
+    fn implements_linearized(&self) -> bool;
+}
+
+impl GetCameraModel for MarsImage {
+    fn get_camera_model(&self) -> Option<CameraModel> {
+        if let Some(right_md) = &self.metadata {
+            if right_md.camera_model_component_list.is_valid() {
+                Some(right_md.camera_model_component_list.clone())
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+
+    fn has_camera_model(&self) -> bool {
+        match self.get_camera_model() {
+            Some(_) => true,
+            None => false
+        }
+    }
+
+    fn implements_linearized(&self) -> bool {
+        match self.get_camera_model() {
+            Some(c) => {
+                if let Ok(_) = c.linearize(self.image.width, self.image.height, self.image.width, self.image.height) {
+                    true
+                } else {
+                    false
+                }
+            },
+            None => false
+        }
+    }
+}
+
+
+fn simple_create(left_img:&MarsImage, right_img:&MarsImage, map:&mut RgbImage) {
+    vprintln!("Adding images");
+    map.paste(&right_img.image, 0, 0);
+    map.paste(&left_img.image, left_img.image.width, 0);
+    map.paste(&right_img.image, left_img.image.width * 2, 0);
+}
+
+
+fn project_line_sample(img:&RgbImage, input_model:&CameraModel, output_model:&Cahv, line:usize, sample:usize, map:&mut RgbImage, x_offset:usize) {
+    let ground = Vector::new(0.0, 0.0, -1.84566);
+
+    if let Ok(lv) = input_model.ls_to_look_vector(&ImageCoordinate{line: line as f64, sample: sample as f64}) {
+        let ls_in = match lv.intersect_to_plane(&ground) {
+            Some(ray) => {
+                output_model.xyz_to_ls(&ray, false)
+            },
+            None => {
+                output_model.xyz_to_ls(&lv.look_direction, true)
+            }
+        };
+
+        let in_x = ls_in.sample.round() as usize;
+        let in_y = ls_in.line.round() as usize;
+
+        if ls_in.sample >= 0.0 && ls_in.line >= 0.0 && in_x < img.width - 1 && in_y < img.height - 1 {
+            let tl = Point::create(
+                sample as f64 + x_offset as f64,
+                line as f64,
+                img.get_band(0).get(in_x, in_y).unwrap() as f64,
+                img.get_band(1).get(in_x, in_y).unwrap() as f64,
+                img.get_band(2).get(in_x, in_y).unwrap() as f64
+            );
+
+            let bl = Point::create(
+                sample as f64 + x_offset as f64,
+                (line + 1) as f64,
+                img.get_band(0).get(in_x, in_y).unwrap() as f64,
+                img.get_band(1).get(in_x, in_y).unwrap() as f64,
+                img.get_band(2).get(in_x, in_y).unwrap() as f64
+            );
+
+            let tr = Point::create(
+                (sample + 1) as f64 + x_offset as f64,
+                line as f64,
+                img.get_band(0).get(in_x, in_y).unwrap() as f64,
+                img.get_band(1).get(in_x, in_y).unwrap() as f64,
+                img.get_band(2).get(in_x, in_y).unwrap() as f64
+            );
+            
+            let br = Point::create(
+                (sample + 1) as f64 + x_offset as f64,
+                (line + 1) as f64,
+                img.get_band(0).get(in_x, in_y).unwrap() as f64,
+                img.get_band(1).get(in_x, in_y).unwrap() as f64,
+                img.get_band(2).get(in_x, in_y).unwrap() as f64
+            );
+
+
+            map.paint_square(&tl, &bl, &br, &tr, false, Eye::DontCare);
+        }
+
+    }
+}
+
+fn linearize_create(left_img:&MarsImage, right_img:&MarsImage, map:&mut RgbImage) {
+    // TODO: This.
+
+    let left_input_cahv = left_img.get_camera_model().expect("No left CAHV after all...");
+    let left_output_cahv = left_input_cahv.linearize(left_img.image.width, left_img.image.height, left_img.image.width, left_img.image.height).expect("Failed to linearize left CAHV");
+    
+    let right_input_cahv = right_img.get_camera_model().expect("No right CAHV after all...");
+    let right_output_cahv = right_input_cahv.linearize(right_img.image.width, right_img.image.height, right_img.image.width, right_img.image.height).expect("Failed to linearize right CAHV");
+
+    for y in 0..left_img.image.height {
+        for x in 0..left_img.image.width {
+            project_line_sample(&right_img.image, &right_input_cahv, &right_output_cahv, y, x, map, 0);
+            project_line_sample(&left_img.image, &left_input_cahv, &left_output_cahv, y, x, map, left_img.image.width);
+            project_line_sample(&right_img.image, &right_input_cahv, &right_output_cahv, y, x, map, left_img.image.width * 2);
+        }
+    }
+
+
+
+    //simple_create(left_img, right_img, map);
+}
+
 impl RunnableSubcommand for CrossEye {
     fn run(&self) {
         print::print_experimental();
@@ -101,11 +229,14 @@ impl RunnableSubcommand for CrossEye {
         let out_height = left_img.image.height + 56;
         let mut map = RgbImage::create(out_width, out_height);
 
-        vprintln!("Adding images");
-        map.paste(&right_img.image, 0, 0);
-        map.paste(&left_img.image, left_img.image.width, 0);
-        map.paste(&right_img.image, left_img.image.width * 2, 0);
-
+        if left_img.implements_linearized() && right_img.implements_linearized() {
+            vprintln!("Both images support CAHV linearization. Taking that path");
+            linearize_create(&left_img, &right_img, &mut map);
+        } else {
+            vprintln!("One or both images support CAHV linearization. Doing simple assembly");
+            simple_create(&left_img, &right_img, &mut map);
+        }
+        
 
         vprintln!("Adding X icon");
         let x_icon = RgbImage::open_from_bytes(&include_bytes!("icons/Xicon.png").to_vec());
