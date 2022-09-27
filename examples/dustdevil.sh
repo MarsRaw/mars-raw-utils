03b1553f2d8ac52f7225e2378137328aee704304
Kevin M. Gill <kmsmgill@gmail.com>
2022-09-27 08:01:12 -0700

    Updated processing for NCAM00594. Made it a little cleaner and allow for two runs of the sequence on the same sol. Added comments as to why this sequence is done a little differently

--- examples/dustdevil.sh
+++ examples/dustdevil.sh
@@ -16,8 +16,7 @@ fi
 
 cd $sol/ECAM 
 
-
-mru msl-fetch -c NAV_RIGHT  -s $sol -n
+mru msl-fetch -c NAV_RIGHT NAV_LEFT  -s $sol -n
 
 mru calibrate -i *JPG -t 2.0
 
@@ -27,6 +26,37 @@ fi
 
 mv *rjcal* RDR
 cd RDR
+
+
+
+# NCAM00594, Dust Devil Survey 3x8 360deg, takes three pictures then rotates for eight pointings.
+# We need to be careful that each differential is done on the three images from the same pointing. 
+# To make sure we do that, we'll only run the differential if we get all 24 (3 * 8) images.
+# If the sequence is run twice on the same sol (3 * 16 = 48), we'll allow them to all go into the same gif.
+# This also needs ImageMagick to merge each pointing gif into a single gif. If that's not found we
+# will level each part seperated which is more clutter but acceptable. 
+num_parts_ncam00594=0
+num_frames_ncam00594=`ls *NCAM00594*-rjcal.png 2> /dev/null | wc -l`
+if [ $num_frames_ncam00594 -eq 48  ]; then
+    num_parts_ncam00594=16
+elif [ $num_frames_ncam00594 -eq 24 ]; then 
+    num_parts_ncam00594=8
+fi
+
+if [ $num_parts_ncam00594 -gt 0 ]; then
+    p=1
+    n=3
+    while [ $p -le $num_parts_ncam00594 ]; do
+        mru -v diffgif -i `ls *NCAM00594*-rjcal.png | head -n $n | tail -n 3` -o DustDevil_${sol}_NCAM00594_part${p}_rjcal.gif -b 0 -w 2.0 -g 2.5 -l 5 -d 40 -p stacked
+        let p=$p+1
+        let n=$n+3
+    done
+    if [ -f /usr/bin/convert ]; then
+        /usr/bin/convert DustDevil_${sol}_NCAM00594_part*_rjcal.gif  DustDevil_${sol}_NCAM00594_rjcal.gif 
+        rm DustDevil_${sol}_NCAM00594_part*_rjcal.gif 
+    fi
+fi
+
 
 if [ `ls *NCAM00593*-rjcal.png 2> /dev/null | wc -l` -gt 0 ]; then
     mru -v diffgif -i NRB*NCAM00593*-rjcal.png -o DustDevil_${sol}_NCAM00593_rjcal.gif -b 0 -w 2.0 -g 2.5 -l 5 -d 20 -p stacked
