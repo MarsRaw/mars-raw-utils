08e62ad2d41f3319587038ecce2493de31c03b5a
Kevin M. Gill <kmsmgill@gmail.com>
2022-12-29 13:52:44 -0800

    Allowing for cross-file decorrelation range determination

--- bin/subs/decorr.rs
+++ bin/subs/decorr.rs
@@ -1,8 +1,10 @@
+use crate::subs::runnable::RunnableSubcommand;
 use mars_raw_utils::prelude::*;
 use rayon::prelude::*;
 use sciimg::prelude::*;
-
-use crate::subs::runnable::RunnableSubcommand;
+use sciimg::MinMax;
+use std::path::PathBuf;
+use std::process;
 
 #[derive(clap::Args)]
 #[clap(author, version, about = "Decorrelation stretching", long_about = None)]
@@ -15,10 +17,24 @@ pub struct DecorrelationStretch {
         multiple_values(true)
     )]
     input_files: Vec<std::path::PathBuf>,
+    #[clap(
+        long,
+        short,
+        help = "Cross-File decorrelation (value ranges determined across all files rather than individually)"
+    )]
+    cross_file: bool,
 }
 
 trait NormalizeRgbImageSingleChannels {
     fn normalize_to(&mut self, to_min: f32, to_max: f32);
+    fn normalize_band_to_with_min_max(
+        &mut self,
+        band: usize,
+        to_min: f32,
+        to_max: f32,
+        from_min: f32,
+        from_max: f32,
+    );
 }
 
 impl NormalizeRgbImageSingleChannels for RgbImage {
@@ -34,29 +50,111 @@ impl NormalizeRgbImageSingleChannels for RgbImage {
             );
         }
     }
+
+    fn normalize_band_to_with_min_max(
+        &mut self,
+        band: usize,
+        to_min: f32,
+        to_max: f32,
+        from_min: f32,
+        from_max: f32,
+    ) {
+        self.set_band(
+            &self
+                .get_band(band)
+                .normalize_force_minmax(to_min, to_max, from_min, from_max)
+                .unwrap(),
+            band,
+        );
+    }
+}
+
+fn cross_file_decorrelation(input_files: &Vec<PathBuf>) {
+    let mut ranges = vec![
+        MinMax {
+            min: std::f32::MAX,
+            max: std::f32::MIN,
+        },
+        MinMax {
+            min: std::f32::MAX,
+            max: std::f32::MIN,
+        },
+        MinMax {
+            min: std::f32::MAX,
+            max: std::f32::MIN,
+        },
+    ];
+
+    vprintln!("Computing value ranges...");
+    input_files.iter().for_each(|in_file| {
+        if in_file.exists() {
+            let image =
+                RgbImage::open(&String::from(in_file.as_os_str().to_str().unwrap())).unwrap();
+
+            for b in 0..3 {
+                let mm = image.get_band(b).get_min_max();
+                ranges[b].min = min!(mm.min, ranges[b].min);
+                ranges[b].max = max!(mm.max, ranges[b].max);
+            }
+        } else {
+            eprintln!("File not found: {:?}", in_file);
+            process::exit(1);
+        }
+    });
+
+    input_files.par_iter().for_each(|in_file| {
+        if in_file.exists() {
+            vprintln!("Processing File: {:?}", in_file);
+
+            let mut image =
+                RgbImage::open(&String::from(in_file.as_os_str().to_str().unwrap())).unwrap();
+
+            for b in 0..3 {
+                image.normalize_band_to_with_min_max(b, 0.0, 65535.0, ranges[b].min, ranges[b].max);
+            }
+
+            image.set_mode(ImageMode::U16BIT);
+
+            vprintln!("Writing to disk...");
+            image.save(&util::append_file_name(
+                in_file.as_os_str().to_str().unwrap(),
+                "decorr",
+            ));
+        } else {
+            eprintln!("File not found: {:?}", in_file);
+        }
+    });
+}
+
+fn individual_file_decorrelation(input_files: &Vec<PathBuf>) {
+    input_files.par_iter().for_each(|in_file| {
+        if in_file.exists() {
+            vprintln!("Processing File: {:?}", in_file);
+
+            let mut image =
+                RgbImage::open(&String::from(in_file.as_os_str().to_str().unwrap())).unwrap();
+
+            image.normalize_to(0.0, 65535.0);
+
+            image.set_mode(ImageMode::U16BIT);
+
+            vprintln!("Writing to disk...");
+            image.save(&util::append_file_name(
+                in_file.as_os_str().to_str().unwrap(),
+                "decorr",
+            ));
+        } else {
+            eprintln!("File not found: {:?}", in_file);
+        }
+    });
 }
 
 #[async_trait::async_trait]
 impl RunnableSubcommand for DecorrelationStretch {
     async fn run(&self) {
-        self.input_files.par_iter().for_each(|in_file| {
-            if in_file.exists() {
-                vprintln!("Processing File: {:?}", in_file);
-
-                let mut image =
-                    RgbImage::open(&String::from(in_file.as_os_str().to_str().unwrap())).unwrap();
-
-                image.normalize_to(0.0, 65535.0);
-                image.set_mode(ImageMode::U16BIT);
-
-                vprintln!("Writing to disk...");
-                image.save(&util::append_file_name(
-                    in_file.as_os_str().to_str().unwrap(),
-                    "decorr",
-                ));
-            } else {
-                eprintln!("File not found: {:?}", in_file);
-            }
-        });
+        match self.cross_file {
+            true => cross_file_decorrelation(&self.input_files),
+            false => individual_file_decorrelation(&self.input_files),
+        };
     }
 }
