3fe885323a73235ac3cf069e5a2201f8c0643dec
Kevin M. Gill <kevin.m.gill@jpl.nasa.gov>
2023-03-01 14:17:32 -0800

    Enabled some ignored tests

--- tests/calibfile.rs
+++ tests/calibfile.rs
@@ -1,11 +1,8 @@
 use mars_raw_utils::calibfile;
 
 #[test]
-#[ignore]
 fn test_load_caldata_mapping_file() {
-    let caldata_toml = calibfile::locate_calibration_file(&String::from("caldata.toml")).unwrap();
-    assert_eq!(caldata_toml, "mars-raw-utils-data/caldata/caldata.toml");
-
+    calibfile::locate_calibration_file(&String::from("caldata.toml")).expect("Failed to locate caldata.toml");
     let config = calibfile::load_caldata_mapping_file().unwrap();
     assert_eq!(
         config.msl.mahli.inpaint_mask,
@@ -14,12 +11,10 @@ fn test_load_caldata_mapping_file() {
 }
 
 #[test]
-#[ignore]
 fn test_locate_without_extention() {
-    let caldata_toml = calibfile::locate_calibration_file_no_extention(
+    calibfile::locate_calibration_file_no_extention(
         &"caldata".to_string(),
         &".toml".to_string(),
     )
-    .unwrap();
-    assert_eq!(caldata_toml, "mars-raw-utils-data/caldata/caldata.toml");
+    .expect("Failed to locate caldata.toml");
 }
--- tests/nsytremote.rs
+++ tests/nsytremote.rs
@@ -1,15 +1,11 @@
 use mars_raw_utils::nsyt::{latest::LatestData, remote::fetch_latest, remote::remote_fetch};
 
 #[tokio::test]
-#[ignore]
-async fn test_msl_latest() {
-    let latest: LatestData = fetch_latest().await.expect("Failed to fetch latest data");
-    assert_eq!(latest.latest, "2022-02-14T15:11:15Z");
-    assert_eq!(latest.latest_sols.len(), 1);
+async fn test_nsyt_latest() {
+    fetch_latest().await.expect("Failed to fetch latest data");
 }
 
 #[tokio::test]
-#[ignore] // Ignoring this by default to prevent unneccessary load on NASA's servers
 async fn test_nsyt_instrument_fetches() {
     let instruments = vec!["idc", "icc"];
     let f: Vec<String> = vec![];
