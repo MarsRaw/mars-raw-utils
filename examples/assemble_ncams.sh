f9a1ca86dbd74cb3395c44ad06aabec0f12ee86d
Kevin M. Gill <kevin.m.gill@jpl.nasa.gov>
2023-03-21 10:10:44 -0700

    Someday I'll remember that I added cargo fmt and clippy to the build. Updated example script assemble_ncams.sh to allow for front and rear hazcams

--- examples/assemble_ncams.sh
+++ examples/assemble_ncams.sh
@@ -6,15 +6,31 @@
 
 ONLY_SCLK=
 CLEANUP=1
-if [ "x$1" != "x" ]; then
-    ONLY_SCLK=$1
-fi
+
+CAM=NCAM
+CAM1L=N
+while [ $# -gt 0 ]; do
+    if [ $1 == "-ncam" ]; then  # Default
+        CAM=NCAM
+        CAM1L=N
+    elif [ $1 == "-fhaz" ]; then
+        CAM=FHAZ
+        CAM1L=F
+    elif [ $1 == "-rhaz" ]; then
+        CAM=RHAZ
+        CAM1L=R
+    elif [ $1 == "-sclk" -a "x$2" != "x" ]; then
+        ONLY_SCLK=$2
+        shift
+    fi
+    shift
+done
 
 function composite_sclk() {
     sclk=$1
     for eye in L R; do
-        prefix=N${eye}F
-        num_parts=`ls ${prefix}*${sclk}*J0?.png | wc -l`
+        prefix=${CAM1L}${eye}F
+        num_parts=`ls ${prefix}*${sclk}*J0?.png 2> /dev/null | wc -l`
         if [ $num_parts -ge 2 ]; then
             output_image=`ls ${prefix}*${sclk}*J0?.png | head -n 1 | sed -e 's/_01_/_00_/' | sed -e 's/.png/-assembled.png/'`
             mru -v m20-ecam-assemble -i ${prefix}*${sclk}*J0?.png -o $output_image
@@ -23,7 +39,7 @@ function composite_sclk() {
 }
 
 if [ "x$ONLY_SCLK" == "x" ];then 
-    for sclk in `ls N{R,L}F_*NCAM*J0?.png | cut -c 10-19 | sort | uniq`; do
+    for sclk in `ls ${CAM1L}{R,L}F_*${CAM}*J0?.png | cut -c 10-19 | sort | uniq`; do
         composite_sclk $sclk
     done
 else 
