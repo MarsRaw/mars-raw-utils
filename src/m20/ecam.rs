95465a50f88886dc0b78afa72c8d938d68561058
Kevin M. Gill <kevin.m.gill@jpl.nasa.gov>
2023-06-13 11:50:04 -0700

    Issue #41: Added bias subtraction to M20 ECAM. Flat is now png for navcams.

--- src/m20/ecam.rs
+++ src/m20/ecam.rs
@@ -2,27 +2,23 @@ use crate::{
     calibfile, calibrate::*, calprofile::CalProfile, decompanding, enums, enums::Instrument,
     marsimage::MarsImage, memcache::load_image, util,
 };
-
 use anyhow::Result;
-use sciimg::{image::Image, path, prelude::ImageBuffer};
+use sciimg::{image::Image, path};
 
 #[derive(Copy, Clone)]
 pub struct M20EECam {}
 
-// Converts an image mask with values 0-255 to 0, 1
-fn create_adjusted_mask(buffer: &ImageBuffer) -> ImageBuffer {
-    let mut adjusted = buffer.clone();
-    (0..adjusted.buffer.len()).for_each(|i| {
-        if adjusted.buffer[i] > 200.0 {
-            // We're bias towards darkening here to account for interpolated pixel values
-            // following an image resize (scale_factor>1). I'd rather grow the dark area (zeros)
-            // then grow the light area.
-            adjusted.buffer[i] = 1.0;
-        } else {
-            adjusted.buffer[i] = 0.0;
-        }
-    });
-    adjusted
+// This really shouldn't be hardcoded
+fn get_bias_for_instrument(instrument: Instrument) -> Result<f32> {
+    match instrument {
+        Instrument::M20NavcamLeft => Ok(133.0),
+        Instrument::M20NavcamRight => Ok(132.0),
+        Instrument::M20FrontHazLeft => Ok(151.0),
+        Instrument::M20FrontHazRight => Ok(148.0),
+        Instrument::M20RearHazLeft => Ok(118.0),
+        Instrument::M20RearHazRight => Ok(132.0),
+        _ => Err(anyhow!("Invalid M20 ECAM instrument: {:?}", instrument)),
+    }
 }
 
 impl Calibration for M20EECam {
@@ -46,7 +42,7 @@ impl Calibration for M20EECam {
     ) -> Result<CompleteContext> {
         let out_file = util::append_file_name(input_file, cal_context.filename_suffix.as_str());
         if path::file_exists(&out_file) && only_new {
-            vprintln!("Output file exists, skipping. ({})", out_file);
+            info!("Output file exists, skipping. ({})", out_file);
             return cal_warn(cal_context, &out_file);
         }
 
@@ -84,12 +80,8 @@ impl Calibration for M20EECam {
 
         let mut raw = MarsImage::open(String::from(input_file), instrument);
 
-        // Apply destretching based off histogram gaps
-        //vprintln!("Destretching...");
-        //raw.destretch_image();
-
         let data_max = if cal_context.apply_ilt {
-            vprintln!("Decompanding...");
+            info!("Decompanding...");
             let lut = decompanding::get_ilt_for_instrument(instrument).unwrap();
             raw.decompand(&lut);
             lut.max() as f32
@@ -99,11 +91,15 @@ impl Calibration for M20EECam {
 
         // Looks like 'ECM' in the name seems to indicate that it still have the bayer pattern
         if raw.image.is_grayscale() {
-            vprintln!("Debayering...");
+            info!("Debayering...");
             raw.debayer_with_method(cal_context.debayer_method);
         }
 
-        vprintln!("Flatfielding...");
+        let bias = get_bias_for_instrument(instrument)?;
+        info!("Applying Bias Subtraction of {}...", bias);
+        raw.image.apply_bias_subtraction(bias);
+
+        info!("Flatfielding...");
         let scale_factor = if let Some(md) = raw.metadata.clone() {
             md.scale_factor
         } else {
@@ -111,89 +107,84 @@ impl Calibration for M20EECam {
         };
         let scale_factor_str = format!("sf{}", scale_factor);
 
-        // let mut flat = flatfield::load_flat(instrument).unwrap();
-        let mut flat = match calibfile::get_calibration_file_for_instrument(
+        let flat = match calibfile::get_calibration_file_for_instrument(
             instrument,
             enums::CalFileType::FlatField,
         ) {
             Ok(s) => {
                 let flat_file_path = s.replace("-scalefactor-", scale_factor_str.as_str());
-                vprintln!(
+                info!(
                     "Flat file path for scale factor {}: {}",
-                    scale_factor,
-                    flat_file_path
+                    scale_factor, flat_file_path
                 );
                 load_image(&flat_file_path).unwrap()
             }
             Err(why) => {
-                vprintln!(
+                warn!(
                     "Flat file not determined for instrument {:?}: {:?}",
-                    instrument,
-                    why
+                    instrument, why
                 );
                 Image::new_empty().unwrap()
             }
         };
 
-        vprintln!("Loading image mask");
-        let mut mask = match calibfile::get_calibration_file_for_instrument(
-            instrument,
-            enums::CalFileType::Mask,
-        ) {
-            Ok(s) => {
-                let mask_file_path = s.replace("-scalefactor-", scale_factor_str.as_str());
-                vprintln!(
-                    "Mask file path for scale factor {}: {}",
-                    scale_factor,
-                    mask_file_path
-                );
-                load_image(&mask_file_path).unwrap()
-            }
-            Err(why) => {
-                vprintln!(
-                    "Flat file not determined for instrument {:?}: {:?}",
-                    instrument,
-                    why
-                );
-                Image::new_empty().unwrap()
-            }
-        };
+        // vprintln!("Loading image mask");
+        // let mut mask = match calibfile::get_calibration_file_for_instrument(
+        //     instrument,
+        //     enums::CalFileType::Mask,
+        // ) {
+        //     Ok(s) => {
+        //         let mask_file_path = s.replace("-scalefactor-", scale_factor_str.as_str());
+        //         info!(
+        //             "Mask file path for scale factor {}: {}",
+        //             scale_factor, mask_file_path
+        //         );
+        //         load_image(&mask_file_path).unwrap()
+        //     }
+        //     Err(why) => {
+        //         warn!(
+        //             "Flat file not determined for instrument {:?}: {:?}",
+        //             instrument, why
+        //         );
+        //         Image::new_empty().unwrap()
+        //     }
+        // };
 
-        if let Some(md) = raw.metadata.clone() {
-            if let Some(rect) = &md.subframe_rect {
-                flat.crop(
-                    (rect[0] as usize - 1) / scale_factor as usize,
-                    (rect[1] as usize - 1) / scale_factor as usize,
-                    (rect[2] as usize) / scale_factor as usize,
-                    (rect[3] as usize) / scale_factor as usize,
-                );
+        // if let Some(md) = raw.metadata.clone() {
+        //     if let Some(rect) = &md.subframe_rect {
+        //         flat.crop(
+        //             (rect[0] as usize - 1) / scale_factor as usize,
+        //             (rect[1] as usize - 1) / scale_factor as usize,
+        //             (rect[2] as usize) / scale_factor as usize,
+        //             (rect[3] as usize) / scale_factor as usize,
+        //         );
 
-                if !mask.is_empty() {
-                    mask.crop(
-                        (rect[0] as usize - 1) / scale_factor as usize,
-                        (rect[1] as usize - 1) / scale_factor as usize,
-                        (rect[2] as usize) / scale_factor as usize,
-                        (rect[3] as usize) / scale_factor as usize,
-                    );
-                }
+        //         if !mask.is_empty() {
+        //             mask.crop(
+        //                 (rect[0] as usize - 1) / scale_factor as usize,
+        //                 (rect[1] as usize - 1) / scale_factor as usize,
+        //                 (rect[2] as usize) / scale_factor as usize,
+        //                 (rect[3] as usize) / scale_factor as usize,
+        //             );
+        //         }
 
-                vprintln!("Flat cropped to {}x{}", flat.width, flat.height);
-            }
-        }
+        //         info!("Flat cropped to {}x{}", flat.width, flat.height);
+        //     }
+        // }
 
-        if !mask.is_empty() {
-            let mask_adjusted = create_adjusted_mask(mask.get_band(0));
-            raw.image
-                .set_band(&raw.image.get_band(0).multiply(&mask_adjusted).unwrap(), 0);
-            raw.image
-                .set_band(&raw.image.get_band(1).multiply(&mask_adjusted).unwrap(), 1);
-            raw.image
-                .set_band(&raw.image.get_band(2).multiply(&mask_adjusted).unwrap(), 2);
-        }
+        // if !mask.is_empty() {
+        //     let mask_adjusted = create_adjusted_mask(mask.get_band(0));
+        //     raw.image
+        //         .set_band(&raw.image.get_band(0).multiply(&mask_adjusted).unwrap(), 0);
+        //     raw.image
+        //         .set_band(&raw.image.get_band(1).multiply(&mask_adjusted).unwrap(), 1);
+        //     raw.image
+        //         .set_band(&raw.image.get_band(2).multiply(&mask_adjusted).unwrap(), 2);
+        // }
         raw.apply_flat(&flat);
 
         if !raw.image.is_grayscale() {
-            vprintln!("Applying color weights...");
+            info!("Applying color weights...");
             raw.apply_weight(
                 cal_context.red_scalar,
                 cal_context.green_scalar,
@@ -202,10 +193,10 @@ impl Calibration for M20EECam {
         }
 
         if cal_context.decorrelate_color {
-            vprintln!("Normalizing with decorrelated colors...");
+            info!("Normalizing with decorrelated colors...");
             raw.image.normalize_to_16bit_decorrelated();
         } else {
-            vprintln!("Normalizing with correlated colors...");
+            info!("Normalizing with correlated colors...");
             raw.image.normalize_to_16bit_with_max(data_max);
         }
 
@@ -230,7 +221,7 @@ impl Calibration for M20EECam {
         match raw.save(&out_file) {
             Ok(_) => cal_ok(cal_context, &out_file),
             Err(why) => {
-                veprintln!("Error saving file: {}", why);
+                error!("Error saving file: {}", why);
                 cal_fail(cal_context, &out_file)
             }
         }
