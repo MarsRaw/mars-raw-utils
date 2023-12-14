fcbf118b9806f937e89377e2a3f8dc0754e62782
Kevin M. Gill <kmsmgill@gmail.com>
2023-12-14 10:08:36 -0800

    Issue #61: Adding CSV output option

--- bin/subs/m20/m20weather.rs
+++ bin/subs/m20/m20weather.rs
@@ -2,44 +2,74 @@ use crate::subs::runnable::RunnableSubcommand;
 use anyhow::Result;
 use clap::Parser;
 use cli_table::{Cell, Style, Table};
-use mars_raw_utils::m20::weather;
+use mars_raw_utils::m20::weather::{self, MedaSol};
 
 #[derive(Parser)]
 #[command(author, version, about = "Return weather data from previous sols", long_about = None)]
-pub struct M20Weather {}
+pub struct M20Weather {
+    #[arg(long, short, help = "Print CSV format")]
+    csv: bool,
+}
+
+fn print_csv(meda_list: &[MedaSol]) {
+    println!("Date,Sol,Max C,Min C,Pressure,Sunrise,Sunset,Season");
+    meda_list.into_iter().for_each(|w| {
+        println!(
+            "{},{},{},{},{},{},{},{}",
+            w.terrestrial_date,
+            w.sol,
+            w.max_temp,
+            w.min_temp,
+            w.pressure,
+            w.sunrise,
+            w.sunset,
+            w.season
+        );
+    });
+}
+
+fn print_table(meda_list: &[MedaSol]) {
+    let table = meda_list
+        .into_iter()
+        .map(|w| {
+            vec![
+                w.terrestrial_date.cell(),
+                w.sol.cell(),
+                w.max_temp.cell(),
+                w.min_temp.cell(),
+                w.pressure.cell(),
+                w.sunrise.clone().cell(),
+                w.sunset.clone().cell(),
+                w.season.clone().cell(),
+            ]
+        })
+        .collect::<Vec<_>>()
+        .table()
+        .title(vec![
+            "Date".cell().bold(true),
+            "Sol".cell().bold(true),
+            "Max (˚C)".cell().bold(true),
+            "Min (˚C)".cell().bold(true),
+            "Pressure (Pa)".cell().bold(true),
+            "Sunrise".cell().bold(true),
+            "Sunset".cell().bold(true),
+            "Season".cell().bold(true),
+        ]);
+
+    println!("{}", &table.display().unwrap());
+}
 
 #[async_trait::async_trait]
 impl RunnableSubcommand for M20Weather {
     async fn run(&self) -> Result<()> {
-        let table = weather::fetch_weather()
-            .await?
-            .into_iter()
-            .map(|w| {
-                vec![
-                    w.terrestrial_date.cell(),
-                    w.sol.cell(),
-                    w.max_temp.cell(),
-                    w.min_temp.cell(),
-                    w.pressure.cell(),
-                    w.sunrise.clone().cell(),
-                    w.sunset.clone().cell(),
-                    w.season.clone().cell(),
-                ]
-            })
-            .collect::<Vec<_>>()
-            .table()
-            .title(vec![
-                "Date".cell().bold(true),
-                "Sol".cell().bold(true),
-                "Max (˚C)".cell().bold(true),
-                "Min (˚C)".cell().bold(true),
-                "Pressure (Pa)".cell().bold(true),
-                "Sunrise".cell().bold(true),
-                "Sunset".cell().bold(true),
-                "Season".cell().bold(true),
-            ]);
+        let meda_list = weather::fetch_weather().await?;
 
-        println!("{}", &table.display().unwrap());
+        if self.csv {
+            print_csv(&meda_list);
+        } else {
+            print_table(&meda_list);
+        }
+
         println!("Source: Mars Environmental Dynamics Analyzer (MEDA)");
         println!("Credit: NASA/JPL-Caltech/CAB(CSIC-INTA)");
         Ok(())
--- bin/subs/msl/mslweather.rs
+++ bin/subs/msl/mslweather.rs
@@ -2,13 +2,67 @@ use crate::subs::runnable::RunnableSubcommand;
 use anyhow::Result;
 use clap::Parser;
 use cli_table::{Cell, Style, Table};
-use mars_raw_utils::msl::weather;
+use mars_raw_utils::msl::weather::{self, RemsSol};
 
 #[derive(Parser)]
 #[command(author, version, about = "Return weather data from previous sols", long_about = None)]
 pub struct MslWeather {
     #[arg(long, short = 'a', help = "Print all results")]
     all: bool,
+
+    #[arg(long, short, help = "Print CSV format")]
+    csv: bool,
+}
+
+fn print_csv(rems_list: &[&RemsSol]) {
+    println!("Date,Sol,Max C,Min C,Pressure,Sunrise,Sunset,Opacity,Season");
+    rems_list.into_iter().for_each(|w| {
+        println!(
+            "{},{},{},{},{},{},{},{},{}",
+            w.terrestrial_date,
+            w.sol,
+            w.max_temp,
+            w.min_temp,
+            w.pressure,
+            w.sunrise,
+            w.sunset,
+            w.atmo_opacity,
+            w.season
+        );
+    });
+}
+
+fn print_table(rems_list: &[&RemsSol]) {
+    let table = rems_list
+        .into_iter()
+        .map(|w| {
+            vec![
+                w.terrestrial_date.cell(),
+                w.sol.cell(),
+                w.max_temp.cell(),
+                w.min_temp.cell(),
+                w.pressure.cell(),
+                w.sunrise.clone().cell(),
+                w.sunset.clone().cell(),
+                w.atmo_opacity.clone().cell(),
+                w.season.clone().cell(),
+            ]
+        })
+        .collect::<Vec<_>>()
+        .table()
+        .title(vec![
+            "Date".cell().bold(true),
+            "Sol".cell().bold(true),
+            "Max (˚C)".cell().bold(true),
+            "Min (˚C)".cell().bold(true),
+            "Pressure (Pa)".cell().bold(true),
+            "Sunrise".cell().bold(true),
+            "Sunset".cell().bold(true),
+            "Opacity".cell().bold(true),
+            "Season".cell().bold(true),
+        ]);
+
+    println!("{}", &table.display().unwrap());
 }
 
 #[async_trait::async_trait]
@@ -21,36 +75,12 @@ impl RunnableSubcommand for MslWeather {
             rems_list.iter().take(10).rev().collect()
         };
 
-        let table = limited_rems_list
-            .into_iter()
-            .map(|w| {
-                vec![
-                    w.terrestrial_date.cell(),
-                    w.sol.cell(),
-                    w.max_temp.cell(),
-                    w.min_temp.cell(),
-                    w.pressure.cell(),
-                    w.sunrise.clone().cell(),
-                    w.sunset.clone().cell(),
-                    w.atmo_opacity.clone().cell(),
-                    w.season.clone().cell(),
-                ]
-            })
-            .collect::<Vec<_>>()
-            .table()
-            .title(vec![
-                "Date".cell().bold(true),
-                "Sol".cell().bold(true),
-                "Max (˚C)".cell().bold(true),
-                "Min (˚C)".cell().bold(true),
-                "Pressure (Pa)".cell().bold(true),
-                "Sunrise".cell().bold(true),
-                "Sunset".cell().bold(true),
-                "Opacity".cell().bold(true),
-                "Season".cell().bold(true),
-            ]);
+        if self.csv {
+            print_csv(&limited_rems_list);
+        } else {
+            print_table(&limited_rems_list);
+        }
 
-        println!("{}", &table.display().unwrap());
         println!("Source: Rover Environmental Monitoring Station (REMS)");
         println!("Credit: NASA/JPL-Caltech/CAB(CSIC-INTA)");
         Ok(())
